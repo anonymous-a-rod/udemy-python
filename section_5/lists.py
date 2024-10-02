18b328562ece6afb7d78ebc18f1e75d92ffb0841
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2024-10-02 16:44:16 -0400

    section 3/4

---
+++ section_5/lists.py
@@ -0,0 +1,10 @@
+parts = ["part 1", "part 2", "part 3", "part 4", "part 5", "part 6", "part 7", "part 8"]
+
+for part in parts:
+    print(part)
+
+print(parts[1])
+print(parts[-1])
+print(parts[0:2])
+print(parts[:3])
+print(parts[::2])
